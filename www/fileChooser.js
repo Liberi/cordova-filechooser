13404df529d3434309f619245446e9266bfbd3b1
Bruno Peres <bruno@geosapiens.com.br>
2016-01-12 14:25:01 -0200

    Added 'mimetypes' parameter to 'open' function

--- www/fileChooser.js
+++ www/fileChooser.js
@@ -1,5 +1,5 @@
 module.exports = {
-    open: function (success, failure) {
-        cordova.exec(success, failure, "FileChooser", "open", []);
+    open: function (success, failure, mimes) {
+        cordova.exec(success, failure, "FileChooser", "open", [mimes]);
     }
 };
